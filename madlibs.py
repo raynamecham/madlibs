e05bb375608dc062ccf404b3f82c352112048aa5
Rayna Mecham <rayna.mecham@gmail.com>
2021-02-27 14:30:19 -0700

    worked on madlibs game

--- madlibs.py
+++ madlibs.py
@@ -41,6 +41,26 @@ def greet_person():
                            person=player,
                            compliment=compliment)
 
+@app.route('/game')
+def show_madlib_form():
+
+    play_game = request.args.get("play")
+
+    if play_game == 'yes':
+        return render_template("game.html")
+    else:
+        return render_template("goodbye.html")
+
+@app.route('/madlib')
+def show_madlib_form():
+
+    name = request.args.get("name")
+    color = request.args.get("color")
+    noun = request.args.get("noun")
+    adjective = request.args.get("adjective")
+
+    return render_template("madlib.html", name = name, color = color, noun = noun, adjective = adjective)
+
 
 if __name__ == '__main__':
     # Setting debug=True gives us error messages in the browser and also
